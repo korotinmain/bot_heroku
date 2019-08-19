f1706cb89dbb37a3de46f0099d0241947a02a242
korotin <denisk@strivtech.com>
2019-08-19 18:57:28 +0300

    Added prod url

--- global-env.js
+++ global-env.js
@@ -1,14 +1,17 @@
-const MAIN_CONV_ID = '-302362122'; //'-1001260986848'; 
-const TOKEN = "479215318:AAENNSIFasERmQy5i4rve48latNkAPejOwo";
+const MAIN_CONV_ID = '-1001260986848'; //'-302362122'; //
+const TOKEN = "976221027:AAFrvjiUKpS3_pI5vuCJJ1ooKb47mO66dp4";
 const MESSAGES = [
     ['Кто сегодня счастливчик?', 'Военный спутник запущен, коды доступа внутри...', 'Кто бы мог подумать, но гусь дня ты -'],
     ['Опять в эти ваши игрульки играете? Ну ладно...', 'Хм...', 'Ведётся захват подозреваемого...', 'Анализ завершен. Ты гусь,'],
     ['Итак... кто же сегодня гусь дня?', 'Где-же он...', 'Проверяю данные...', 'Ну ты и гусь,']
 ];
 
-const CONNECT_STRING = "mongodb://KorotinDenysBot:11223344q4@ds113738.mlab.com:13738/telegram_bot";
+const PROD_URL = "https://prod-telegram-bot.herokuapp.com";
+
+const CONNECT_STRING = "mongodb://KorotinDenysBot:11223344q4@ds117158.mlab.com:17158/telegram_bot_testing";
 
 module.exports.MAIN_CONV_ID = MAIN_CONV_ID;
 module.exports.MESSAGES = MESSAGES;
 module.exports.TOKEN = TOKEN;
 module.exports.CONNECT_STRING = CONNECT_STRING;
+module.exports.PROD_URL = PROD_URL;
